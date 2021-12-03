296f5da1ca07cc2d4942aad6520e77cce0e5a9e8
sneha <ss4547@columbia.edu>
2021-12-03 17:23:48 -0500

    add flags

--- dapg/examples/hydra_job_script.py
+++ dapg/examples/hydra_job_script.py
@@ -50,34 +50,34 @@ def main(flags : DictConfig):
     torch.manual_seed(flags.random_seed)
     logging.info("Running...")
 
-    # assert 'algorithm' in job_data.keys()
-    assert any([job_data.algorithm == a for a in ['NPG', 'BCRL', 'DAPG']])
-    # job_data.lam_0 = 0.0 if 'lam_0' not in job_data.keys() else job_data.lam_0
-    # job_data.lam_1 = 0.0 if 'lam_1' not in job_data.keys() else job_data.lam_1
+    # assert 'algorithm' in flags.job_data.keys()
+    assert any([flags.job_data.algorithm == a for a in ['NPG', 'BCRL', 'DAPG']])
+    # flags.job_data.lam_0 = 0.0 if 'lam_0' not in flags.job_data.keys() else flags.job_data.lam_0
+    # flags.job_data.lam_1 = 0.0 if 'lam_1' not in flags.job_data.keys() else flags.job_data.lam_1
     # EXP_FILE = JOB_DIR + '/job_config.json'
     # with open(EXP_FILE, 'w') as f:
-    #     json.dump(job_data, f, indent=4)
-    # logging.info(json.dump(job_data, f, indent=4))
+    #     json.dump(flags.job_data, f, indent=4)
+    # logging.info(json.dump(flags.job_data, f, indent=4))
 
     # ===============================================================================
     # Train Loop
     # ===============================================================================
-    wandb.init(project=args.project,group=job_data.algorithm,config=job_data, reinit=True)
+    wandb.init(project=args.project,group=flags.job_data.algorithm,config=flags.job_data, reinit=True)
 
-    e = GymEnv(job_data.env)
-    policy = MLP(e.spec, hidden_sizes=job_data.policy_size, seed=job_data.seed)
-    baseline = MLPBaseline(e.spec, reg_coef=1e-3, batch_size=job_data.vf_batch_size,
-                           epochs=job_data.vf_epochs, learn_rate=job_data.vf_learn_rate)
+    e = GymEnv(flags.job_data.env)
+    policy = MLP(e.spec, hidden_sizes=flags.job_data.policy_size, seed=flags.job_data.seed)
+    baseline = MLPBaseline(e.spec, reg_coef=1e-3, batch_size=flags.job_data.vf_batch_size,
+                           epochs=flags.job_data.vf_epochs, learn_rate=flags.job_data.vf_learn_rate)
 
     # Get demonstration data if necessary and behavior clone
-    if job_data.algorithm != 'NPG':
+    if flags.job_data.algorithm != 'NPG':
         print("========================================")
         print("Collecting expert demonstrations")
         print("========================================")
-        demo_paths = pickle.load(open(job_data.demo_file, 'rb'))
+        demo_paths = pickle.load(open(flags.job_data.demo_file, 'rb'))
 
-        bc_agent = BC(demo_paths, policy=policy, epochs=job_data.bc_epochs, batch_size=job_data.bc_batch_size,
-                      lr=job_data.bc_learn_rate, loss_type='MSE', set_transforms=False)
+        bc_agent = BC(demo_paths, policy=policy, epochs=flags.job_data.bc_epochs, batch_size=flags.job_data.bc_batch_size,
+                      lr=flags.job_data.bc_learn_rate, loss_type='MSE', set_transforms=False)
         in_shift, in_scale, out_shift, out_scale = bc_agent.compute_transformations()
         bc_agent.set_transformations(in_shift, in_scale, out_shift, out_scale)
         bc_agent.set_variance_with_data(out_scale)
@@ -92,11 +92,11 @@ def main(flags : DictConfig):
         print("time taken = %f" % (timer.time() - ts))
         print("========================================")
 
-        if job_data.eval_rollouts >= 1:
-            score = e.evaluate_policy(policy, num_episodes=job_data.eval_rollouts, mean_action=True)
+        if flags.job_data.eval_rollouts >= 1:
+            score = e.evaluate_policy(policy, num_episodes=flags.job_data.eval_rollouts, mean_action=True)
             print("Score with behavior cloning = %f" % score[0][0])
 
-    if job_data.algorithm != 'DAPG':
+    if flags.job_data.algorithm != 'DAPG':
         # We throw away the demo data when training from scratch or fine-tuning with RL without explicit augmentation
         demo_paths = None
 
@@ -105,9 +105,9 @@ def main(flags : DictConfig):
     # ===============================================================================
 
     rl_agent = DAPG(e, policy, baseline, demo_paths,
-                    normalized_step_size=job_data.rl_step_size,
-                    lam_0=job_data.lam_0, lam_1=job_data.lam_1,
-                    seed=job_data.seed, save_logs=True
+                    normalized_step_size=flags.job_data.rl_step_size,
+                    lam_0=flags.job_data.lam_0, lam_1=flags.job_data.lam_1,
+                    seed=flags.job_data.seed, save_logs=True
                     )
 
     print("========================================")
@@ -117,15 +117,15 @@ def main(flags : DictConfig):
     ts = timer.time()
     train_agent(job_name=JOB_DIR,
                 agent=rl_agent,
-                seed=job_data.seed,
-                niter=job_data.rl_num_iter,
-                gamma=job_data.rl_gamma,
-                gae_lambda=job_data.rl_gae,
-                num_cpu=job_data.num_cpu,
+                seed=flags.job_data.seed,
+                niter=flags.job_data.rl_num_iter,
+                gamma=flags.job_data.rl_gamma,
+                gae_lambda=flags.job_data.rl_gae,
+                num_cpu=flags.job_data.num_cpu,
                 sample_mode='trajectories',
-                num_traj=job_data.rl_num_traj,
-                save_freq=job_data.save_freq,
-                evaluation_rollouts=job_data.eval_rollouts)
+                num_traj=flags.job_data.rl_num_traj,
+                save_freq=flags.job_data.save_freq,
+                evaluation_rollouts=flags.job_data.eval_rollouts)
     print("time taken = %f" % (timer.time()-ts))
 
 
